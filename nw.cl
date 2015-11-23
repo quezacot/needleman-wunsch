18beadfcd3e3e12c0935edffddd3fa26ae2783de
Yj Cheng <cheng.yungjen@gmail.com>
2015-11-23 01:03:28 -0500

    block version

--- nw.cl
+++ nw.cl
@@ -29,7 +29,7 @@ initialize_table(__global unsigned int *table,
         }
     }
 }
-
+/*
 __kernel void
 needleman_by1(__global __read_only char* seq1,
               __global __read_only char* seq2,
@@ -49,6 +49,62 @@ needleman_by1(__global __read_only char* seq1,
             ++cur;
         }
         table[to1D(w, x, y)] = min( pre, cur );
+    }
+
+    // Make sure all threads reach the next part after
+    // the local buffer is loaded
+    //barrier(CLK_LOCAL_MEM_FENCE);
+}
+*/
+__kernel void
+needleman_byblock(__global __read_only char* seq1,
+                  __global __read_only char* seq2,
+                  __global unsigned int *table,
+                  __local int *buffer,
+                  unsigned int iter,
+                  int w, int h,
+                  int buf_w, int buf_h,
+                  int edge)
+{
+     // Global position of output pixel
+    const unsigned int x = get_global_id(0);
+    const unsigned int y = get_global_id(1);
+    // Load the relevant labels to a local buffer with a halo
+    if ( x > 0 && y > 0 && x%(buf_w-edge) == 1 && y%(buf_h-edge) == 1 && x/(buf_w-edge) + y/(buf_h-edge) == iter ) {
+        printf("iter:%u, x:%u, y:%u\n", iter, x, y);
+        // load to local buffer
+        for( int i=0; i<buf_w && x+i-edge<w; ++i ){
+            buffer[to1D(buf_w, i, 0)] = table[to1D(w, x+i-edge, y-edge)];
+        }
+        for( int j=edge; j<buf_h && y+j-edge<h; ++j ){
+            buffer[to1D(buf_w, 0, j)] = table[to1D(w, x-edge, y+j-edge)];
+        }
+        
+        // do serial NW in local memory
+        for( int j=edge; j<buf_h && y+j-edge<h; ++j ){
+            for( int i=edge; i<buf_w && x+i-edge<w; ++i ){
+                //printf("u:%u, l:%u\n", x-1+i-edge, y-1+j-edge);
+                //printf("u:%c, l:%c\n", seq1[x-1+i-edge], seq2[y-1+j-edge]);
+                //printf("take min, l:%u, u:%u\n", buffer[to1D(buf_w, i-1, j)], buffer[to1D(buf_w, i, j-1)] );
+                buffer[to1D(buf_w, i, j)] = min( buffer[to1D(buf_w, i-1, j)]+1, buffer[to1D(buf_w, i, j-1)]+1 );
+                //printf("temp min: %u\n", buffer[to1D(buf_w, i, j)]);
+                int cur = buffer[to1D(buf_w, i-1, j-1)];
+                //printf("lu: %u\n", cur);
+                if( seq1[x-1+i-edge] != seq2[y-1+j-edge] ){
+                    ++cur;
+                }
+                buffer[to1D(buf_w, i, j)] = min( buffer[to1D(buf_w, i, j)], cur );
+                //printf("%u\n", buffer[to1D(buf_w, i, j)]);
+            }
+        }
+        
+        // copy local buffer back to global
+        for( int j=1; j<buf_h && y+j-edge<h; ++j ){
+            for( int i=1; i<buf_w && x+i-edge<w; ++i ){
+                table[to1D(w, x+i-edge, y+j-edge)] = buffer[to1D(buf_w, i, j)];
+            }
+        }
+
     }
 
     // Make sure all threads reach the next part after
--- nw.py
+++ nw.py
@@ -2,6 +2,7 @@ from __future__ import division
 import sys
 import pyopencl as cl
 import numpy as np
+import math
 import pylab
 import string
 import time
@@ -96,12 +97,13 @@ if __name__ == '__main__':
                 # TCTGGTCAGGTGGGCAAACCGACCGATCCACGGAGGGAAG"
     string1 = "CCTTGCTACGCACGGGCACGGAGCGCAGCCCCAGCCACCCCTAATCACACA"
     string2 = "CCGGCCCCGGAACGGTTTGCACCTGGGAATCAGCGCCGCCTCGGCCGATAA"
+    #string1 = "CCTGGCTAC"
+    #string2 = "CCGGCCTAAC"
     seq1 = np.fromstring(string1, dtype='|S1')
     seq2 = np.fromstring(string2, dtype='|S1')
     
     len1 = len(seq1)+1
     len2 = len(seq2)+1
-    sz = min(len1, len2)
     
     host_table = np.zeros(len1*len2, dtype=np.uint32)
     #print host_table.reshape([len2,len1])
@@ -116,23 +118,28 @@ if __name__ == '__main__':
     cl.enqueue_copy(queue, gpu_seq2_buff, seq2, is_blocking=False)
     queue.finish()
     
-    local_size = (1, sz)  # ?? per work group
+    if len1 < len2:
+        local_size = (len1, 1)  # ?? per work group
+    else:
+        local_size = (1, len2)
     global_size = (len1, len2)
-    #print global_size
+    print global_size
+    print local_size
     width = np.int32(len1)
     height = np.int32(len2)
+    edge = np.int32(1)
     
     # Create a local memory per working group that is
     # the size of an int (4 bytes) * (N+2) * (N+2), where N is the local_size
-    #buf_size = (np.int32(local_size[0] + 2 * halo), np.int32(local_size[1] + 2 * halo))
-    #gpu_local_memory = cl.LocalMemory(4 * buf_size[0] * buf_size[1])
+    buf_size = (np.int32(8 + edge), np.int32(12 + edge))
+    gpu_local_memory = cl.LocalMemory(4 * buf_size[0] * buf_size[1])
 
     # initialize labels
     program.initialize_table(queue, global_size, local_size,
                              dptable, width, height)
 
     # while not done, propagate labels
-    itercount = len1 + len2 - 1
+    itercount = np.int32(math.ceil((len1-1.0)/(buf_size[0]-1.0)) + math.ceil((len2-1.0)/(buf_size[1]-1.0)))
     show_progress = False
 
     # Show the initial labels
@@ -141,14 +148,17 @@ if __name__ == '__main__':
     
     total_time = 0
 
-    for itr in xrange(2,itercount):
+    for itr in xrange(itercount):
         #host_done_flag[0] = 0
         #cl.enqueue_copy(queue, gpu_done_flag, host_done_flag, is_blocking=False)
-        prop_exec = program.needleman_by1(queue, global_size, local_size,
-                                          gpu_seq1_buff, gpu_seq2_buff,
-                                          dptable,
-                                          np.int32(itr),
-                                          width, height)
+        prop_exec = program.needleman_byblock(queue, global_size, local_size,
+                                              gpu_seq1_buff, gpu_seq2_buff,
+                                              dptable,
+                                              gpu_local_memory,
+                                              np.int32(itr),
+                                              width, height,
+                                              buf_size[0], buf_size[1],
+                                              edge)
         prop_exec.wait()
         elapsed = 1e-6 * (prop_exec.profile.end - prop_exec.profile.start)
         total_time += elapsed
@@ -159,12 +169,14 @@ if __name__ == '__main__':
     # Show final result
     cl.enqueue_copy(queue, host_table, dptable, is_blocking=True)
     
+    print "Parallel result:"
+    print host_table.reshape([len2,len1])
     s_time = time.time()
     serial = edit_distance(string1, string2)
+    print "Serial result:"
+    print serial
     s_time = time.time() - s_time
     assert (host_table.reshape([len2,len1]) == serial).all()
-    #print host_table.reshape([len2,len1])
-    #print edit_distance(string1, string2)
     print('Parallel time: {}'.format(total_time) )
     print('Serial time: {}'.format(s_time) )
     
